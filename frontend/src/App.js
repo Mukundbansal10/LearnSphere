6b2654a6a869f981474b2335d4da5e6207051d7c
Mukund70 <mukundbansal10@gmail.com>
2024-12-24 22:17:34 +0530

    Initial frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from 'axios';
+import { useEffect } from 'react';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+useEffect(() => {
+axios.get('http://localhost:5000').then(response => {
+console.log(response.data);
+});
+}, []);
+return (
+<div>
+<h1>Welcome to E-Learning Platform</h1>
+</div>
+);
 }
-
 export default App;
