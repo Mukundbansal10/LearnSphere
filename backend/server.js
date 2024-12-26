57735e6b0bce5a06e8faa96ca739de09dba9373c
GargKrishna24 <gargkrishna888@gmail.com>
2024-12-26 22:26:45 +0530

    rename courses.js to course.js in model folder

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+//const Course = require('../models/Course');
+
 
 
 dotenv.config();
